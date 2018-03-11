5b220f55c3f522596dcfc50858f534bf16f37948
Stanley Huang <infamous_godhand@yahoo.com>
2018-03-11 04:55:57 +0000

    implemented axios GET request to get json

--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Selection from "./selection.js";
 import Flag from "./flag.js";
-import "./main.css"
+import "./main.css";
+import axios from 'axios';
 
 var imgURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUINP1UMay2F4mgO9vfgMtcyzb3NQRXjmFp8CdZOTNtQPfwdHkfA";
 
 class Main extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            countries:[]
+        }
+    }
     
     componentDidMount(){
-        console.log("mounted");
+        var url= 'https://restcountries.eu/rest/v2/all';
+        axios.get(url)
+        .then(function (response) {
+         this.setState({countries:response.data});
+         }.bind(this));
     }
     
     render(){
