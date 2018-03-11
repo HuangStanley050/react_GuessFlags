99f44794fab080507226c3f3f874f5d2f45e892a
Stanley Huang <infamous_godhand@yahoo.com>
2018-03-11 10:28:43 +0000

    game logic done, able to select right and wrong

--- src/components/selection.js
+++ src/components/selection.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./selection.css";
 
 
+/*
+// 0 -> 10
+Math.floor(Math.random() * 11);
+*/
+var answer="stuff";
+var selections=[];
 class Selection extends React.Component{
     constructor(props){
         super(props);
@@ -12,28 +18,43 @@ class Selection extends React.Component{
     }
     handleClick(e){
         var answer=e.target.value;
-       
+        
         this.setState({choice:answer});
+       if(answer!== this.state.answer){
+           alert("incorrect");
+       }
+       else{
+           alert("correct");
+       }
         
        
     }
-    componentWillReceiveProps(nextProp){
-        alert(nextProp.correctAnswer.name);
-        this.setState({answer:nextProp.correctAnswer.name});
+    componentWillReceiveProps(nextProps){
+        //alert(nextProps.correctAnswer.name);
+        var first = Math.floor(Math.random()*251);//--->get random country for the incorrect answers
+        selections.push(nextProps.name[first].name.substring(0,15));
+        
+        var second= Math.floor(Math.random()*251);
+        selections.push(nextProps.name[second].name.substring(0,15));
+        
+        var third = Math.floor(Math.random()*251);
+        selections.push(nextProps.name[third].name.substring(0,15));
+        //alert(selections);
+        this.setState({answer:nextProps.correctAnswer.name.substring(0,15)});
+        
+        
         
     }
     
     render(){
-        
-        
         return (
             <div className="selectWrapper">
                     
-                <input type="radio" onClick={this.handleClick} value="answer1"name="answer"/> An1
-                <input type="radio" onClick={this.handleClick} value="answer2"name="answer"/> An2
-                <input type="radio" onClick={this.handleClick} value="answer3"name="answer"/> An3
-                <input type="radio" onClick={this.handleClick} value="answer4"name="answer"/> An4
-                <input type="button" value="Guess" />
+            <input type="radio" onClick={this.handleClick} value={this.state.answer} name="answer"/> {this.state.answer}
+            <input type="radio" onClick={this.handleClick} value={selections[0]} name="answer"/> {selections[0]}
+            <input type="radio" onClick={this.handleClick} value={selections[1]} name="answer"/> {selections[1]}
+            <input type="radio" onClick={this.handleClick} value={selections[2]} name="answer"/> {selections[2]}
+            <input type="button" value="Guess" />
                     
         
             </div>
